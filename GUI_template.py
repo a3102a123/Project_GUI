3f97a7bf7e84f6a694cc6fc87578116653d8eedd
a3102a123 <a3102a123@gmail.com>
2020-07-11 17:50:32 +0800

    add the button to draw BF match line

--- GUI_template.py
+++ GUI_template.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(1511, 869)
+        Dialog.resize(1731, 869)
         self.img_label1 = QtWidgets.QLabel(Dialog)
         self.img_label1.setGeometry(QtCore.QRect(70, 20, 681, 681))
         self.img_label1.setText("")
@@ -45,8 +45,62 @@ class Ui_Dialog(object):
         self.img_text1.setGeometry(QtCore.QRect(260, 710, 256, 41))
         self.img_text1.setObjectName("img_text1")
         self.SIFT_Button = QtWidgets.QCheckBox(Dialog)
-        self.SIFT_Button.setGeometry(QtCore.QRect(680, 770, 85, 19))
+        self.SIFT_Button.setGeometry(QtCore.QRect(1450, 240, 85, 19))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.SIFT_Button.setFont(font)
         self.SIFT_Button.setObjectName("SIFT_Button")
+        self.textBrowser = QtWidgets.QTextBrowser(Dialog)
+        self.textBrowser.setGeometry(QtCore.QRect(1450, 90, 191, 41))
+        self.textBrowser.setObjectName("textBrowser")
+        self.textBrowser_2 = QtWidgets.QTextBrowser(Dialog)
+        self.textBrowser_2.setGeometry(QtCore.QRect(1450, 190, 191, 41))
+        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.textBrowser_3 = QtWidgets.QTextBrowser(Dialog)
+        self.textBrowser_3.setGeometry(QtCore.QRect(1450, 290, 191, 41))
+        self.textBrowser_3.setObjectName("textBrowser_3")
+        self.spinBox = QtWidgets.QSpinBox(Dialog)
+        self.spinBox.setGeometry(QtCore.QRect(1450, 340, 191, 51))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.spinBox.setFont(font)
+        self.spinBox.setMaximum(100)
+        self.spinBox.setProperty("value", 100)
+        self.spinBox.setObjectName("spinBox")
+        self.widget = QtWidgets.QWidget(Dialog)
+        self.widget.setGeometry(QtCore.QRect(1451, 131, 191, 31))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.BF_Flow_Button = QtWidgets.QCheckBox(self.widget)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.BF_Flow_Button.setFont(font)
+        self.BF_Flow_Button.setObjectName("BF_Flow_Button")
+        self.horizontalLayout.addWidget(self.BF_Flow_Button)
+        self.BF_Line_Button = QtWidgets.QCheckBox(self.widget)
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.BF_Line_Button.setFont(font)
+        self.BF_Line_Button.setObjectName("BF_Line_Button")
+        self.horizontalLayout.addWidget(self.BF_Line_Button)
+        self.widget.raise_()
+        self.img_label1.raise_()
+        self.Right_Button.raise_()
+        self.Left_Button.raise_()
+        self.img_label2.raise_()
+        self.img_text2.raise_()
+        self.img_text1.raise_()
+        self.SIFT_Button.raise_()
+        self.BF_Flow_Button.raise_()
+        self.BF_Line_Button.raise_()
+        self.textBrowser.raise_()
+        self.textBrowser_2.raise_()
+        self.textBrowser_3.raise_()
+        self.spinBox.raise_()
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -67,6 +121,23 @@ class Ui_Dialog(object):
 "</style></head><body style=\" font-family:\'PMingLiU\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; background-color:#ffffff;\"><span style=\" font-size:14pt;\">out_0.jpg</span></p></body></html>"))
         self.SIFT_Button.setText(_translate("Dialog", "SIFT"))
+        self.textBrowser.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'PMingLiU\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Brute force match</span></p></body></html>"))
+        self.textBrowser_2.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'PMingLiU\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Show key points</span></p></body></html>"))
+        self.textBrowser_3.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'PMingLiU\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Distance Limit</span></p></body></html>"))
+        self.BF_Flow_Button.setText(_translate("Dialog", "flow field"))
+        self.BF_Line_Button.setText(_translate("Dialog", "line"))
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     img_dir_path = "im0/"
     img_name_arr = []
     img_kp_arr = []
+    BF_match_arr = []
     img_arr = []
     img_num = 0
 
@@ -30,6 +31,8 @@ if __name__ == "__main__":
         img2.set_img(img_arr, img2_idx , img_kp_arr)
         if ui.SIFT_Button.isChecked():
             draw_SIFT_kp()
+        if ui.BF_Flow_Button.isChecked():
+            draw_BF_match_flow()
         dis_img()
         text1 = img_name_arr[img1.idx]
         text2 = img_name_arr[img2.idx]
@@ -44,11 +47,29 @@ if __name__ == "__main__":
         cv2.drawKeypoints(img2.img,img2.kps,img2_out)
         img1.draw_img(img1_out)
         img2.draw_img(img2_out)
+    
+    # draw the flow of BF match
+    def draw_BF_match_flow():
+        img1_out = copy.deepcopy(img1.img)
+        img2_out = copy.deepcopy(img2.img)
+        matches = BF_match_arr[img1.idx]
+        for m in matches:
+            kp1 = img1.kps[m[0].queryIdx]
+            kp2 = img2.kps[m[0].trainIdx]
+            kp1_pt = np.array( kp1.pt )
+            kp2_pt = np.array( kp2.pt )
+            cv2.arrowedLine(img1_out,(int(kp1_pt[0]),int(kp1_pt[1])),(int(kp2_pt[0]),int(kp2_pt[1])),(0,0,255),1)
+            cv2.arrowedLine(img2_out,(int(kp1_pt[0]),int(kp1_pt[1])),(int(kp2_pt[0]),int(kp2_pt[1])),(0,0,255),1)
+        img1.draw_img(img1_out)
+        img2.draw_img(img2_out)
         
     def button_fun():
         ui.Right_Button.clicked.connect(lambda: change_image(1))
         ui.Left_Button.clicked.connect(lambda: change_image(-1))
         ui.SIFT_Button.clicked.connect(lambda: change_image(0))
+        ui.BF_Flow_Button.clicked.connect(lambda: change_image(0))
+        ui.BF_Line_Button.clicked.connect(lambda: change_image(0))
+    
     # load image
     def load_img(img_name_arr):
         img_name_arr[:] = os.listdir(img_dir_path)
@@ -63,6 +84,7 @@ if __name__ == "__main__":
             if i > 10:
                 break
         return i
+    
     # check whether the data files exist
     def check_data_file():
         dir_path = "data/"
@@ -70,9 +92,9 @@ if __name__ == "__main__":
             import create_data.SIFT_kp
         if not os.path.isfile(dir_path + "SIFT_BF_match.txt"):
             import create_data.SIFT_BF_match
+    
     # load key points data
-    def load_data(img_kp_arr):
-        check_data_file()
+    def load_SIFT_kp():
         data_path = "data/SIFT_kp.txt"
         f = open(data_path,"rb")
         data = cPickle.loads(f.read())
@@ -83,6 +105,28 @@ if __name__ == "__main__":
                                 _response=point[3], _octave=point[4], _class_id=point[5])
                 temp_arr.append(temp)
             img_kp_arr.append(temp_arr)
+        f.close()
+    
+    # load BF match data
+    def load_BF_match():
+        data_path = "data/SIFT_BF_match.txt"
+        f = open(data_path,"rb")
+        data = cPickle.loads(f.read())
+        for match_arr in data:
+            temp_arr = []
+            for m in match_arr:
+                m1 = cv2.DMatch(m[0][0],m[0][1],m[0][2])
+                m2 = cv2.DMatch(m[1][0],m[1][1],m[1][2])
+                temp_arr.append((m1,m2))
+            BF_match_arr.append(temp_arr)
+        f.close()
+    
+    # load data
+    def load_data(img_kp_arr):
+        check_data_file()
+        load_SIFT_kp()
+        load_BF_match()
+    
     # init image entity
     def init():
         img1.set_img(img_arr,0,img_kp_arr)
